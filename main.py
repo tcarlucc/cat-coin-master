90ff8d44961d6e5f7fad852ee923a04e8ce782b1
tcarlucc <tcarlucc@uvm.edu>
2022-11-08 19:56:49 -0500

    Menu Loop implemented

--- main.py
+++ main.py
@@ -4,5 +4,28 @@ CS 166 OL
 Cat Coin Blockchain
 """
 
+def menu_loop() -> int:
+    ans = 0
+
+    print("Please select from the following options:")
+    print("1: Check Wallet Balance")
+    print("2: Transfer CatCoins")
+    print("3: Quit CatCoin")
+
+    while ans == 0:
+        try:
+            option = int(input(">>>"))
+
+            if option < 1 or option > 3:
+                print("Please choose a valid wallet!")
+            else:
+                ans = option
+        except ValueError:
+            print("Please enter a valid integer!")
+
+    return ans
+
+
 if __name__ == "__main__":
-    pass
+    print(menu_loop())
+
